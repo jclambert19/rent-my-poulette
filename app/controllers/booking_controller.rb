b8d5af03189f8a4a9e67d1a1a6dd6ab93e62505b
Jean-Christophe Lambert <iamjclambert@gmail.com>
2015-11-04 10:13:26 +0100

    Jour 2-A

---
+++ app/controllers/booking_controller.rb
@@ -0,0 +1,45 @@
+class BookingController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @booking = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to bookings_path
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to bookings_path
+  end
+
+  def destroy
+    @bookings.delete
+    redirect_to bookings_path
+  end
+
+
+
+private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:????)
+  end
+end
